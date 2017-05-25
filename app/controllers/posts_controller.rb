cd217a6d37ac4d994bc361dd8f83fa01356bed45
VToropov1337 <vladimirtoropov87@gmail.com>
2017-05-26 00:12:18 +0300

    add gems / add Post views/ add methods (new, create,index, show)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,45 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :update, :edit, :destroy]
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :link, :description)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,2 @@
 class Post < ApplicationRecord
-
-def index
-
-end
-
-
-
-
 end
